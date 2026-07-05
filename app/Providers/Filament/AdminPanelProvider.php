101c7be2c7154c8e45e88365089376769810bf0c
zuanPdana <pradanazuan28+github2@gmail.com>
2026-07-05 09:10:26 +0700

    modified:   app/Providers/Filament/AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->brandName('SCRA Nightly')
+            ->brandName('SCRA')
             ->colors([
                 'primary' => Color::Blue,
             ])
